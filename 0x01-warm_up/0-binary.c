fe2f5aa095c34eaf71c62c742b75e84454cc5dd7
GucciGerm <germaine.balmania@gmail.com>
2018-10-08 20:39:48 +0000

    Adding binary search method along with helper function and header file

--- 0x01-warm_up/0-binary.c
+++ 0x01-warm_up/0-binary.c
@@ -1,6 +1,31 @@
 #include "search_algos.h"
 
 /**
+ * print_digit - This helper function will print the array contents with message
+ *
+ * array - is the pointer to the first element of the array to search in
+ * left - this is the left side of the array
+ * right - this is the right side of the array
+ *
+ * Return: void
+ */
+
+void print_digit(int *array, size_t left, size_t right)
+{
+	size_t idx = left;
+
+	printf("Searching in array: ");
+	while (idx < right)
+	{
+		printf("%d, ", array[idx]);
+		idx++;
+	}
+	printf("%d\n", array[idx]);
+}
+
+
+
+/**
  * binary_search - This function will search for value in sorted array of
  * integers using the binary search algorithum
  *
@@ -13,10 +38,30 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-  if (array[value] == NULL || if array == NULL)
-    {
-      return (-1);
-    
-    }
+    size_t left = 0;
+    size_t right = size - 1;
+    size_t mid;
 
+    if (array == NULL)
+    {
+	    return (-1);
+    }
+    while (left <= right)
+    {
+	    mid = ((left + right) / 2);
+	    print_digit(array, left, right);
+	    if (array[mid] < value)
+	    {
+		    left = (mid + 1);
+	    }
+	    else if (array[mid] > value)
+            {
+		    right = (mid);
+	    }
+	    else
+	    {
+		    return (mid);
+	    }
+    }
+    return (-1);
 }
--- 0x01-warm_up/search_algos.h
+++ 0x01-warm_up/search_algos.h
@@ -5,7 +5,6 @@
 #include <stdlib.h>
 
 int binary_search(int *array, size_t size, int value);
-int binary_tree_is_full(const binary_tree_t *tree);
 
 
 #endif
