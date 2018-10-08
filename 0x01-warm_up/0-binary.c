e5f5caa8130089af50eb3d593962757a4c60bb51
GucciGerm <germaine.balmania@gmail.com>
2018-10-08 19:32:35 +0000

    Adding all current content

---
+++ 0x01-warm_up/0-binary.c
@@ -0,0 +1,22 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - This function will search for value in sorted array of
+ * integers using the binary search algorithum
+ *
+ * array: is the pointer to the first element of the array to search in
+ * size: is the number of elements within the array
+ * value: is the value we are searching for
+ *
+ * Return: the index where the value in located
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+  if (array[value] == NULL || if array == NULL)
+    {
+      return (-1);
+    
+    }
+
+}
