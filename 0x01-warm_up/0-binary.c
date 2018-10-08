f60ec83ce03328e93e0b5ade1c1494f79851580c
GucciGerm <germaine.balmania@gmail.com>
2018-10-08 20:56:12 +0000

    fixed betty errors

--- 0x01-warm_up/0-binary.c
+++ 0x01-warm_up/0-binary.c
@@ -1,11 +1,10 @@
 #include "search_algos.h"
 
 /**
- * print_digit - This helper function will print the array contents with message
- *
- * array - is the pointer to the first element of the array to search in
- * left - this is the left side of the array
- * right - this is the right side of the array
+ * print_digit - This helper function will print array contents with message
+ * @array: is the pointer to the first element of the array to search in
+ * @left: this is the left side of the array (start)
+ * @right: this is the right side of the array (end)
  *
  * Return: void
  */
@@ -23,45 +22,42 @@ void print_digit(int *array, size_t left, size_t right)
 	printf("%d\n", array[idx]);
 }
 
-
-
 /**
  * binary_search - This function will search for value in sorted array of
  * integers using the binary search algorithum
- *
- * array: is the pointer to the first element of the array to search in
- * size: is the number of elements within the array
- * value: is the value we are searching for
+ * @array: is the pointer to the first element of the array to search in
+ * @size: is the number of elements within the array
+ * @value: is the value we are searching for
  *
  * Return: the index where the value in located
  */
 
 int binary_search(int *array, size_t size, int value)
 {
-    size_t left = 0;
-    size_t right = size - 1;
-    size_t mid;
+	size_t left = 0;
+	size_t right = size - 1;
+	size_t mid;
 
-    if (array == NULL)
-    {
-	    return (-1);
-    }
-    while (left <= right)
-    {
-	    mid = ((left + right) / 2);
-	    print_digit(array, left, right);
-	    if (array[mid] < value)
-	    {
-		    left = (mid + 1);
-	    }
-	    else if (array[mid] > value)
-            {
-		    right = (mid);
-	    }
-	    else
-	    {
-		    return (mid);
-	    }
-    }
-    return (-1);
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	while (left <= right)
+	{
+		mid = ((left + right) / 2);
+		print_digit(array, left, right);
+		if (array[mid] < value)
+		{
+			left = (mid + 1);
+		}
+		else if (array[mid] > value)
+		{
+			right = (mid);
+		}
+		else
+		{
+			return (mid);
+		}
+	}
+	return (-1);
 }
